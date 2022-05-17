7e5df85613d659521f2839a53c19bfa298dc63d6
Bruno Santos <brunosantosfarias2@gmail.com>
2022-05-17 20:14:58 -0300

    hook de listagem de Usuários

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,4 @@
 import Link from "next/link";
-import { useEffect } from "react";
-import { useQuery } from "react-query";
 
 import {
   Box,
@@ -23,33 +21,10 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import { api } from "../../services/api";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, isRefetching, error } = useQuery(
-    "users",
-    async () => {
-      const { data } = await api.get("users");
-
-      const users = data.users.map((user) => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-            day: "2-digit",
-            month: "long",
-            year: "numeric",
-          }),
-        };
-      });
-
-      return users;
-    },
-    {
-      staleTime: 1000 * 5, // 5 seconds
-    },
-  );
+  const { data, isLoading, isRefetching, error } = useUsers();
 
   console.log(data);
 
